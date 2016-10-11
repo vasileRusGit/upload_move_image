18fe6ce3071240be97c28bc70f26826221fefb79
vasileRusGit <vasileRusGit@gmail.com>
2016-10-11 23:36:35 +0300

    Update play.php

--- play.php
+++ play.php
@@ -28,3 +28,31 @@ while ($row = mysqli_fetch_array($result)) {
     echo "<img src='" . $row['image'] . "'>"; //'image' is the name from the database
 }
 ?>
+if ($page == 'add') {
+    $name = $_POST['name'];
+    $type = $_POST['type'];
+    $price = $_POST['price'];
+    $roast = $_POST['roast'];
+    $country = $_POST['country'];
+    $review = $_POST['review'];
+    $image = $_POST['file'];
+
+    $result2 = explode('.', $image);
+    $name = $result2[count($result2) - 2];
+    $name = explode("\\", $name);
+    $name = $name[count($name) - 1];
+
+    $result = explode('.', $image);
+    $ext = $result[count($result) - 1];
+    $file_name = $name . '.' . $ext;
+    $url = "Images/Coffee/" . $name . '.' . $ext;
+
+    if (in_array($ext, array('gif', 'jpg', 'jpeg', 'png'))) {
+//        if (is_upload ed_file($name)) {
+        move_uploaded_file($file_name, "Images/Coffee/");
+        $query = $databaseConnection->prepare("INSERT INTO coffee(name, type , price, roast, country,image, review, deleted) VALUES('$name', '$type', '$price', '$roast', '$country', '$url', '$review', '0')");
+        $query->execute();
+    }
+//        }
+//}
+
